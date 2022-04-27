bdafb650c989fdb4b1e8255e9409c29d8fd25d47
Trenton Teasdale <dr.teasdale.trenton@gmail.com>
2022-04-27 16:06:20 -0600

    json first use

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,8 @@
-console.log('JS file linked');
+fetch('https://api.chucknorris.io/jokes/random')
+    .then((res) =>{
+        return res.json();
+    })
+    .then((data) => {
+        console.log(data);
+        console.log(data.value);
+    })
