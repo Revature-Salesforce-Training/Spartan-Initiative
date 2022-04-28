6d16763c374c5d2f4caadef5ac7c08d43921d484
Trenton Teasdale <dr.teasdale.trenton@gmail.com>
2022-04-28 12:55:28 -0600

    updated HTML, css, and js files

--- README.md
+++ README.md
@@ -13,14 +13,14 @@ Kanban board that tracks tasks and progress of project
 Maintain standup notes on branches and feature their notes from every standup
 
 WireFrames 
-Before beginning work - create a wireframe mockup of webpage and upload it to to your branch 
+Before beginning work - create a wireframe mockup of webpage and upload it to to your branch - COMPLETE
 
 HTML
 Create a well formed HTML page that illustrates understandng of elements, tags, and attributes
     -must inlcue "Links and Images" - "Tables and lists" - "Block & Inline Elements"
 
 CSS
-Create custom styling for HTML page that demonstrates understanding of box model, flexbox, custom properties and CSS declation and selectors
+Create custom styling for HTML page that demonstrates understanding of box model, flexbox, custom properties and CSS declation and selectors 
 
 JAVASCRIPT 
 Leverage understanding of javascript, develop the requisite code to call out to an external API and perform DOM manipulation based on the result.
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,56 @@
-fetch('https://api.chucknorris.io/jokes/random')
+// Author: Trenton Teasdale
+// Created: 4/27/22
+// Last Modified: 5/1/22
+// This an a weather API able to show todays weather
+
+//Begin fetch for weather API
+fetch('https://api.openweathermap.org/data/2.5/weather?lat=40.302332124&lon=-111.754663648&appid=718e507933c2bfe59ab786b07998f3e5')
     .then((res) =>{
         return res.json();
     })
     .then((data) => {
         console.log(data);
-        console.log(data.value);
+        //weather type secelector for API data
+        let weather = data.weather[0];
+        let weatherIcon = weather.icon;
+        getIcon(weatherIcon);
+        weather = weather.description;
+        //append weather status to current section in HTML
+        let current =document.getElementById('current');
+        current.append(weather);
+
+        //add tempurature
+        let temp = document.getElementById('temp');
+        //Temp comes in as kelvin and needs to be converted to F
+        let kelvin = data.main.temp;
+        let fahrenheit = Math.round(1.8*(kelvin - 273) + 32);
+        temp.append(fahrenheit);
+
+        //adding feels like temp
+        let tempFeel = document.getElementById('feel');
+        let feel = data.main.feels_like;
+        //conversion
+        feel = Math.round(1.8*(feel - 273) + 32);
+        tempFeel.append(feel);
     })
+function setDate() {
+    //Start with getting date
+const date = (new Date()).toLocaleDateString('en-US');
+let dateSection = document.getElementById('date');
+dateSection.append(date);
+}
+//run set date function
+setDate();
+
+function getIcon(weatherIcon) {
+    let iconCode = weatherIcon;
+    let iconUrl = 'https://openweathermap.org/img/wn/' +iconCode +'@2x.png';
+    fetch(iconUrl)
+    .then((arg) => {
+        let wIcon = new Image();
+        wIcon.src = arg.url;
+        let weatherImg = document.getElementById('weather');
+        weatherImg.append(wIcon);
+    })
+}
+// 
